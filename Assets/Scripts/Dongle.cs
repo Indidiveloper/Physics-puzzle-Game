6da930f16e4fbc4205bc3f6913dc3210318f0e95
MINUK <zoow2@naver.com>
2023-05-07 11:54:17 +0900

    Add object pooling system to optimize object allocation and reusee

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -33,6 +33,26 @@ public class Dongle : MonoBehaviour
         anim.SetInteger("Level", level);
     }
 
+    void OnDisable()
+    {
+        // 동글 속성 초기화
+        level = 0;
+        isDrag = false;
+        isMerge = false;
+        isAttach = false;
+
+        // 동글 트랜스폼 초기화
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.zero;
+
+        // 동글 물리 초기화
+        rigid.simulated = false;
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+        circle.enabled = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
